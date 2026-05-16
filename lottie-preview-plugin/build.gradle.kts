460445e278f9079f4d4a5e6ddcbf9fcad949ce66
SekhGulamMainuddin <sekhgmainuddin@gmail.com>
2026-05-16 22:09:21 +0530

    chore: set up IntelliJ plugin build

---
+++ lottie-preview-plugin/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
+    id("org.jetbrains.intellij.platform") version "2.10.4"
+}
+
+group = "com.lottiepreview"
+version = providers.gradleProperty("pluginVersion").get()
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        local("/Applications/Android Studio.app")
+        bundledPlugin("org.jetbrains.android")
+    }
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "com.lottiepreview.plugin"
+        name = "Lottie Preview"
+        version = providers.gradleProperty("pluginVersion").get()
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceBuild").get()
+            untilBuild = providers.gradleProperty("pluginUntilBuild").get()
+        }
+
+        vendor {
+            name = "Lottie Preview"
+        }
+
+        description = """
+            Preview Lottie animations directly inside Android Studio using the embedded JCEF browser.
+        """.trimIndent()
+    }
+
+    pluginVerification {
+        ides {
+            local("/Applications/Android Studio.app")
+        }
+    }
+}
